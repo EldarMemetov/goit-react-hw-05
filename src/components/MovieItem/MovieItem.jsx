77881c8dd486167ed03bb9807a8278119c435e76
EldarMemetov <eldarm9347@gmail.com>
2024-08-06 16:53:39 +0200

    update

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -1,12 +1,19 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { imageBaseURL } from "../../components/ApiMovies/ApiMovies.jsx";
+import styles from "./MovieItem.module.css";
 
 export default function MovieItem({ movie }) {
-  const location = useLocation();
-
   return (
-    <div>
-      <Link to={`/movies/${movie.id}`} state={location}>
-        <h4>{movie.title}</h4>
+    <div className={styles.movieItem}>
+      {movie.poster_path && (
+        <img
+          src={`${imageBaseURL}${movie.poster_path}`}
+          alt={movie.title}
+          className={styles.moviePoster}
+        />
+      )}
+      <Link to={`/movies/${movie.id}`} className={styles.movieLink}>
+        <button className={styles.movieButton}>Go to movie</button>
       </Link>
     </div>
   );
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,10 +1,12 @@
-import MovieItem from "../MovieItem/MovieItem";
+
+import MovieItem from '../MovieItem/MovieItem';
+import styles from './MoviesList.module.css';
 
 export default function MoviesList({ movies }) {
   return (
-    <ul>
+    <ul className={styles.moviesList}>
       {movies.map((movie) => (
-        <li key={movie.id}>
+        <li key={movie.id} className={styles.movieListItem}>
           <MovieItem movie={movie} />
         </li>
       ))}
